284749516b0717f699cd57e3a853f0659db04b9b
Mehrab Bohlooli <bohlool82@gmail.com>
2022-05-22 22:14:02 +0430

    Add user

---
+++ src/main/java/ir/ac/kntu/models/User.java
@@ -0,0 +1,72 @@
+package ir.ac.kntu.models;
+
+public class User {
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String nationalCode;
+
+    private String phone;
+
+    public User(String name, String username, String password, String email, String nationalCode, String phone) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.nationalCode = nationalCode;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
