e11757635d23a5e47ff6a84462abff9a34267ea7
Mehrab Bohlooli <bohlool82@gmail.com>
2022-05-30 14:53:54 +0430

    Add an admin user

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,9 +1,16 @@
 package ir.ac.kntu;
 
 import ir.ac.kntu.core.Console;
+import ir.ac.kntu.data.Users;
+import ir.ac.kntu.models.User;
 
 public class Main {
     public static void main(String[] args) {
+        seed();
         Console.listen();
+    }
+
+    private static void seed() {
+        Users.getInstance().add(new User("admin", "admin", "pass", "a@b.com", "0123456789", "09123456789"));
     }
 }
