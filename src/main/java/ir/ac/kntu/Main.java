91a0a650291aa611ab24713fbccbca2396e30c61
Mehrab Bohlooli <bohlool82@gmail.com>
2022-05-31 17:22:53 +0430

    Add some class and another user in seed function

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,7 +1,10 @@
 package ir.ac.kntu;
 
+import ir.ac.kntu.core.Auth;
 import ir.ac.kntu.core.Console;
+import ir.ac.kntu.data.Classrooms;
 import ir.ac.kntu.data.Users;
+import ir.ac.kntu.models.Classroom;
 import ir.ac.kntu.models.User;
 
 public class Main {
@@ -12,5 +15,10 @@ public class Main {
 
     private static void seed() {
         Users.getInstance().add(new User("admin", "admin", "pass", "a@b.com", "0123456789", "09123456789"));
+        Users.getInstance().add(new User("user", "user", "pass", "b@b.com", "0123456781", "09123156789"));
+        Auth.login("admin", "pass");
+        Classrooms.getInstance().add(new Classroom("class1", "institute1", "instructor1", 1401, true, null));
+        Classrooms.getInstance().add(new Classroom("class2", "institute2", "instructor2", 1401, true, "pass"));
+        Auth.logout();
     }
 }
