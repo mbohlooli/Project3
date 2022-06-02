062a985695c96a854bb942d81fcf7dca6c773e45
Mehrab Bohlooli <bohlool82@gmail.com>
2022-06-02 11:12:18 +0430

    Add Question model

---
+++ src/main/java/ir/ac/kntu/models/question/Difficulty.java
@@ -0,0 +1,8 @@
+package ir.ac.kntu.models.question;
+
+public enum Difficulty {
+    Simple,
+    Average,
+    Hard,
+    VeryHard
+}
---
+++ src/main/java/ir/ac/kntu/models/question/Question.java
@@ -0,0 +1,89 @@
+package ir.ac.kntu.models.question;
+
+import ir.ac.kntu.core.Auth;
+import ir.ac.kntu.models.User;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+public abstract class Question {
+    protected String name;
+
+    protected int maxScore;
+
+    protected String description;
+
+    protected User owner;
+
+    protected Date createdAt;
+
+    protected String answer;
+
+    protected boolean autoCheck;
+
+    protected Difficulty difficulty;
+
+    public Question(String name, int maxScore, String description, String answer, boolean autoCheck, Difficulty difficulty) {
+        this.name = name;
+        this.maxScore = maxScore;
+        this.description = description;
+        this.answer = answer;
+        this.autoCheck = autoCheck;
+        this.difficulty = difficulty;
+        owner = Auth.getCurrentUser();
+        createdAt = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxScore() {
+        return maxScore;
+    }
+
+    public void setMaxScore(int maxScore) {
+        this.maxScore = maxScore;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isAutoCheck() {
+        return autoCheck;
+    }
+
+    public void setAutoCheck(boolean autoCheck) {
+        this.autoCheck = autoCheck;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+}
