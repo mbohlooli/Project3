3318f8b7fd053689bf2d323b2c14baf2bbc90354
Mehrab Bohlooli <bohlool82@gmail.com>
2022-06-02 12:04:51 +0430

    Add submission for short answer question

--- src/main/java/ir/ac/kntu/models/question/ShortAnswerQuestion.java
+++ src/main/java/ir/ac/kntu/models/question/ShortAnswerQuestion.java
@@ -1,5 +1,10 @@
 package ir.ac.kntu.models.question;
 
+import ir.ac.kntu.core.Auth;
+import ir.ac.kntu.models.Submission;
+
+import java.util.ArrayList;
+
 public class ShortAnswerQuestion extends Question {
     private boolean autoCheck;
 
@@ -9,6 +14,26 @@ public class ShortAnswerQuestion extends Question {
         super(name, maxScore, description, difficulty);
         this.autoCheck = autoCheck;
         this.answer = answer;
+    }
+
+    @Override
+    public void submitAnswer(Submission submission) {
+        if (!autoCheck) {
+            super.submitAnswer(submission);
+            return;
+        }
+
+        submission.setScore(submission.getAnswer().equals(answer) ? maxScore : 0);
+        ArrayList<Submission> submissions = submissionPacks.get(Auth.getCurrentUser()).getSubmissions();
+        for (Submission s: submissions) {
+            if (s.getScore() > submission.getScore()) {
+                return;
+            } else {
+                s.setFinal(false);
+            }
+        }
+        submission.setFinal(true);
+        submissionPacks.get(Auth.getCurrentUser()).addSubmission(submission);
     }
 
     public boolean isAutoCheck() {
