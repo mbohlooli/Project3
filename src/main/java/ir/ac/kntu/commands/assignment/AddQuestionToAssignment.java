a7dd4bc9549ae1c82b0be9155d86464b4355256a
Mehrab Bohlooli <bohlool82@gmail.com>
2022-06-03 11:00:08 +0430

    Question bank can be sorted based on date and difficulty

--- src/main/java/ir/ac/kntu/commands/assignment/AddQuestionToAssignment.java
+++ src/main/java/ir/ac/kntu/commands/assignment/AddQuestionToAssignment.java
@@ -13,14 +13,14 @@ public class AddQuestionToAssignment extends SecureCommand {
     public void secureExecute() {
         Classroom classroom = Console.nextClassroom(Auth.getCurrentUser().getOwnedClassrooms().stream().toList());
         Assignment assignment = Console.nextAssignment(classroom);
-        System.out.println("Do you want to add question from question bank (y/n)?");
+        System.out.print("Do you want to add question from question bank (y/n)? ");
         boolean importFromQuestionBank = Console.nextBoolean();
         Question question;
         if (importFromQuestionBank) {
             question = Console.nextQuestion(QuestionBank.getInstance().all());
         } else {
             question = Console.nextQuestion();
-            System.out.println("Add to question bank (y/n)?");
+            System.out.print("Add to question bank (y/n)? ");
             boolean addToQuestionBank = Console.nextBoolean();
             if (addToQuestionBank) {
                 QuestionBank.getInstance().add(question);
--- src/main/java/ir/ac/kntu/commands/question/BrowseQuestionBank.java
+++ src/main/java/ir/ac/kntu/commands/question/BrowseQuestionBank.java
@@ -1,11 +1,16 @@
 package ir.ac.kntu.commands.question;
 
 import ir.ac.kntu.commands.SecureCommand;
+import ir.ac.kntu.core.Console;
 import ir.ac.kntu.data.QuestionBank;
 
 public class BrowseQuestionBank extends SecureCommand {
     @Override
     public void secureExecute() {
-        QuestionBank.getInstance().all().forEach(System.out::println);
+        System.out.print("Sort by time (y/n)? (Default is difficulty) ");
+        boolean sortByTime = Console.nextBoolean();
+        QuestionBank.getInstance().all().stream()
+                .sorted((q1,q2) -> (sortByTime) ? q1.getCreatedAt().compareTo(q2.getCreatedAt()) : q1.getDifficulty().compareTo(q2.getDifficulty()))
+                .forEach(System.out::println);
     }
 }
