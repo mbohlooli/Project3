8aa26427c0f0010cd458f4081c0ba039ab452348
Mehrab Bohlooli <bohlool82@gmail.com>
2022-06-03 10:51:37 +0430

    Owner can import or export question from question bank

--- src/main/java/ir/ac/kntu/commands/assignment/AddQuestionToAssignment.java
+++ src/main/java/ir/ac/kntu/commands/assignment/AddQuestionToAssignment.java
@@ -3,6 +3,7 @@ package ir.ac.kntu.commands.assignment;
 import ir.ac.kntu.commands.SecureCommand;
 import ir.ac.kntu.core.Auth;
 import ir.ac.kntu.core.Console;
+import ir.ac.kntu.data.QuestionBank;
 import ir.ac.kntu.models.Assignment;
 import ir.ac.kntu.models.Classroom;
 import ir.ac.kntu.models.question.*;
@@ -12,7 +13,19 @@ public class AddQuestionToAssignment extends SecureCommand {
     public void secureExecute() {
         Classroom classroom = Console.nextClassroom(Auth.getCurrentUser().getOwnedClassrooms().stream().toList());
         Assignment assignment = Console.nextAssignment(classroom);
-        Question question = Console.nextQuestion();
+        System.out.println("Do you want to add question from question bank (y/n)?");
+        boolean importFromQuestionBank = Console.nextBoolean();
+        Question question;
+        if (importFromQuestionBank) {
+            question = Console.nextQuestion(QuestionBank.getInstance().all());
+        } else {
+            question = Console.nextQuestion();
+            System.out.println("Add to question bank (y/n)?");
+            boolean addToQuestionBank = Console.nextBoolean();
+            if (addToQuestionBank) {
+                QuestionBank.getInstance().add(question);
+            }
+        }
         assignment.addQuestion(question);
     }
 }
