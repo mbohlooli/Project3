644bac647401e60ed0cf8533dbefd3c21f83a140
Mehrab Bohlooli <bohlool82@gmail.com>
2022-06-05 19:57:57 +0430

    Style refactoring

--- src/main/java/ir/ac/kntu/core/Patterns.java
+++ src/main/java/ir/ac/kntu/core/Patterns.java
@@ -2,6 +2,8 @@ package ir.ac.kntu.core;
 
 public class Patterns {
     public static final String EMAIL = "^.*@[a-zA-Z]+\\.[a-zA-Z]+$";
+
     public static final String NATIONAL_CODE = "^\\d{10}$";
+
     public static final String PHONE_NUMBER = "^09\\d{9}$";
 }
